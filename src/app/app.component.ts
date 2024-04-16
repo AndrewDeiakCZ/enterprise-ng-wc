9bbdaf96d98b3f828d8b16c9a0c693d3f28f00e9
null <null>
2024-04-16 17:42:38 +0200

    popup menu data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, VERSION } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, VERSION, ViewChild} from '@angular/core';
 import { Router } from '@angular/router'
 import 'ids-enterprise-wc/enterprise-wc.js';
 
@@ -9,16 +9,47 @@ import 'ids-enterprise-wc/enterprise-wc.js';
     './app.component.css'
   ]
 })
-export class AppComponent {
+export class AppComponent implements  AfterViewInit {
+  @ViewChild('popupmenuEl', { read: ElementRef }) popupmenuEl;
   public name = 'Angular ' + VERSION.major;
 
-  list = Array.from({ length: 25 }, (_, index) => "item" + (index + 1));
+  list1 = Array.from({ length: 25 }, (_, index) => "item" + (index + 1));
+
+  list = Array.from({ length: 25 }, (_, index) => "item" + (index + 1)).map((v) => {
+    return {
+      id: v,
+      text: v,
+      click: () => this.itemClick()
+    }
+  });
   
-  constructor(
-    private router: Router
-  ) {}
+
+  listObj = [{
+    id: "edit-actions-group",
+    select: "none",
+    items: [...this.list]
+  }]
 
   itemClick(): void {
     this.list = this.list.splice(10);
+    this.listObj = [{
+      id: "edit-actions-group",
+      select: "none",
+      items: [...this.list]
+    }]
+
+    setTimeout(() => {
+      this.popupmenuEl.nativeElement.popup.align =  'top, left';
+      this.popupmenuEl.nativeElement.data = this.listObj
+    }, 500);
+  }
+
+  ngAfterViewInit(): void {
+    this.popupmenuEl.nativeElement.popup.align =  'top, left';
+    this.popupmenuEl.nativeElement.data = this.listObj;
+    this.popupmenuEl.nativeElement.addEventListener('selected', (e: any) => {
+      console.warn(`Item "${e.detail.elem.text}" was selected (id "${e.detail.elem.id}")`);
+      this.itemClick()
+    });
   }
 }
